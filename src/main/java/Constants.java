1dd567f51998805681e3f9aac4a00f3bb5f1f2ec
yanke <13971098750@163.com>
2020-05-14 09:55:06 +0800

    初始化

---
+++ src/main/java/Constants.java
@@ -0,0 +1,11 @@
+/**
+ * @Auther: yanke
+ * @Date: 2020/5/6 12:41
+ * @Description:
+ */
+public interface Constants {
+    String GROUP_ID = "group.id";
+    String KAFKA_TOPICS = "kafka.topics";
+    String KAFKA_BOOTSTRAP_SERVERS = "bootstrap.servers";
+    String STREAMING_CHECKPOINT_DIR = "streaming.checkpoint.dir";
+}
